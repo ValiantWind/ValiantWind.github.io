9036554e3133f942e0df97d6be483fa6ab3d8c61
ValiantWind69 <83314733+ValiantWind69@users.noreply.github.com>
2022-03-21 10:06:03 -0700

    Create player.js

---
+++ js/player.js
@@ -0,0 +1,35 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '405',
+        width: '720',
+        videoId: 'QtBDL8EiNZo',
+        playerVars: {
+            'autoplay': 1,
+            'controls': 0,
+            'showinfo': 1,
+            'enablejsapi': 1,
+            'loop': 1,
+            'autohide': 1,
+            'playsinline': 1,
+        },
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+    if (player.getPlayerState() != 1) {
+        event.target.mute();
+        event.target.playVideo();
+    }
+}
